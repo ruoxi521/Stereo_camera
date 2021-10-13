022bc9fe9479da34041c1c6116e926edf6fd74ca
若兮 <83598844+ruoxi521@users.noreply.github.com>
2021-10-13 23:02:38 +0800

    fix /test
    fix some bugs

--- src/test/collect_picture.cpp
+++ src/test/collect_picture.cpp
@@ -17,9 +17,9 @@ int main()
 	imshow("【双目原始视图】", frame);
 	cvWaitKey(300);
 	//system("C:\\\"Program Files (x86)\"\\\"CHUSEI 3D Webcam\"\\\"CHUSEI 3D Webcam.exe\"");    //Windows系统下，此处改成你的上位机安装绝对路径
-    system("/home/sujie/Stereo_cam/Camera_switch/camera_switch.sh");  //Ubuntu系统下，此处改成你的脚本存放绝对路径
+    system("../../sh/camera_switch.sh");  //Ubuntu系统下，此处改成你的脚本存放相对路径
 
-	char buf[30] = { 0 }; //保存路径变量
+	char buf[100]; //保存路径变量
 
 	while (1)
 	{
@@ -34,7 +34,6 @@ int main()
 		resize(frame, DoubleImage, Size(640, 240), (0, 0), (0, 0), INTER_AREA);   // 纵向分辨率缩小一半
 		imshow("【双目缩小视图】", DoubleImage);  //显示图像
 
-
 		Mat	LeftImage = DoubleImage(Rect(0, 0, 320, 240));      //分割得到左视图
 		Mat RightImage = DoubleImage(Rect(320, 0, 320, 240));   //分割得到右视图
 
@@ -47,25 +46,22 @@ int main()
 			break;
 		}
 
-		static int i = 9;
-		if (13 == char(c))
+		static int i = 1;
+		if (32 == char(c))  //keycode   32 = space 空格
 		{
-			sprintf(buf, ".\\picture\\left_%d.png", i);  //保存左视图
-			cout << buf;
-			imwrite(buf, LeftImage);
+			sprintf(buf, "./picture/left_%d.png", i);  //保存左视图
+			imwrite(buf, LeftImage);  //如果没有保存图片是由于相对路径设置不正确
+			//imwrite("./picture/left.png",LeftImage);    //一定要注意Linux下的相对路径
 
-
-
-			sprintf(buf, ".\\picture\\right_%d.png", i);  //保存右视图
+			sprintf(buf, "./picture/right_%d.png", i);  //保存右视图
 			imwrite(buf, RightImage);
 
-
-
-			sprintf(buf, ".\\picture\\total_%d.png", i);  //保存整体图像
+			sprintf(buf, "./picture/total_%d.png", i);  //保存整体图像
 			imwrite(buf, DoubleImage);
+			
+			printf("第%d次保存图片\n",i);
 			i++;
 		}
-
 	}
 	cap.release();//释放资源
 }
